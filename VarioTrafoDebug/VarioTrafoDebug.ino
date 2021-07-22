9d6ecf3e6cb551339868e0cea0c568a945807c76
Michel Sommer <skruen@gmail.com>
2021-07-22 15:50:45 +0200

    Added debug method with DC measurement

--- VarioTrafoDebug/VarioTrafoDebug.ino
+++ VarioTrafoDebug/VarioTrafoDebug.ino
@@ -5,7 +5,7 @@
  *    A1 = adj. Current
  *    A2 = not adj. Current
  *    
- * Pin Arduino Pro Micro:
+ * Pin Arduino Pro Micro: 
  *    5 -> RGB led pin R, via R220 resistor, PWM
  *    6 -> RGB led pin G, via R220 resistor, PWM
  *    7 -> Relay, adj. voltage
@@ -70,6 +70,9 @@ int alertCurrent = 2.5;     // Amp
 
 void setup() {
 
+  Serial.begin(115200);
+  Serial.println("Start initializing... ");
+  
   pinMode(rLED, OUTPUT);
   pinMode(gLED, OUTPUT);
   pinMode(bLED, OUTPUT);
@@ -91,19 +94,22 @@ void setup() {
   initLED();
     
   voltageSensor.calibrate();  // Set 0V, voltage sensor must be without voltage
-
+  Serial.println("Voltage sensor calibrated");
   initLED();
   
   adjCurrentSensor.calibrate();  // Set 0A, current must not be flowing in circut
+  Serial.println("Adjustable current sensor calibrated");
 
   initLED();
   
   nAdjCurrentSensor.calibrate();  // Set 0A, current must not be flowing in circut
+  Serial.println("Not-adjustable current sensor calibrated");
 
   // Signal setup is compleated
   initLED();
   rgbLED(0, 0, 255);
-  
+
+  Serial.println("Ended initializing.\n");
 }
 
 void initLED() {
@@ -115,14 +121,17 @@ void initLED() {
 
 void alertLED() {
   rgbLED(255, 0, 0);
+  Serial.println("Alert LED - ON (RED)");
 }
 
 void blueLED() {
   rgbLED(0, 0, 255);
+  Serial.println("Standby LED - ON (Blue)");
 }
 
 void blackLED() {
   rgbLED(0, 0, 0);
+  Serial.println("LED - OFF");
 }
 
 void overload() {
@@ -130,6 +139,7 @@ void overload() {
     digitalWrite(nAdjRelay, LOW);
     alertLED();
     alert = true;
+    Serial.println("Relays OFF, AlertLED; ON - Overload");
 }
 
 void safe() {
@@ -163,7 +173,7 @@ void safe() {
 }
 
 void calcAdjCurrentLimit() {
-  
+  Serial.print("Calculateing adjustable current limit...");
   if(adjU < 30) {
     adjCurrentLimit = 0,25;
   } else if (30 < adjU && adjU < 240 ) {
@@ -171,33 +181,41 @@ void calcAdjCurrentLimit() {
   } else  {
     adjCurrentLimit = 2;
   }
-  
+  Serial.print((String)"adjCurrentLimit: " + adjCurrentLimit + " A");
 }
 
 void colorCalc() {
 float result = 0;
-
+  Serial.print("Calculateing LED color...");
+  Serial.println((String)"bool adjButtonValue: " + adjButtonValue );
+  Serial.println((String)"bool nAdjButtonValue: " + nAdjButtonValue );
+  
   while(adjButtonValue || nAdjButtonValue) {
-        
+   Serial.print("Do nou calculate if adjButtonValue or nAdjButtonValue is = false");     
    if ( adjButtonValue == true && nAdjButtonValue == true ) {
      if( I > maxCurrent && adjI > adjCurrentLimit ) {
        result = ( maxCurrent - adjI ) / 2;
+       Serial.println((String)"adjButtonValue == true && nAdjButtonValue == true => " + result );
      }
    }
    
    if ( adjButtonValue == true ) {
      result = maxCurrent / 2;
+     Serial.println((String)"adjButtonValue == true => " + result );
    }
 
    if ( nAdjButtonValue == true ) {
      result = adjCurrentLimit / 2; 
+     Serial.println((String)"nAdjButtonValue == true => " + result );
    }
 
     // (0, 255, 0) -> (+1, 0, 0) -> (255, 255, 0) -> (255, -1, 0) -> (255, 0, 0)
     if ( result >= 255 ){
       rgbLED( result, 255, 0);
+      Serial.println((String)"rgbLED( " + result + ", 255, 0 )" );
     } else if ( 256 <= result ) {
       rgbLED( 255, 255 - result, 0);
+      Serial.println((String)"rgbLED( 255, " + result + ", 0 )" );
     }
   }
 }
@@ -206,22 +224,43 @@ void rgbLED(int r, int g, int b) {
   analogWrite(rLED, r);
   analogWrite(gLED, g);
   analogWrite(bLED, b);
+  Serial.println((String)"analogWrite(rLED, r) = " + r );
+  Serial.println((String)"analogWrite(gLED, g) = " + g );
+  Serial.println((String)"analogWrite(bLED, b) = " + b );
 }
 
-void messurment() {
-  adjU = voltageSensor.getVoltageAC();
-  adjI = adjCurrentSensor.getCurrentAC();
-  nAdjI = nAdjCurrentSensor.getCurrentAC();
+void messurment() { 
+  Serial.println((String)"pin A0: " + digitalRead(A0) + " V");
+  adjU = voltageSensor.getVoltageDC();
+  Serial.println((String)"adjU: (DC) = " + adjU + " V");
+
+  Serial.println((String)"pin A1: " + digitalRead(A1) + " V");
+  adjI = adjCurrentSensor.getCurrentDC();
+  Serial.println((String)"adjI: (DC) = " + adjI + " A");
+
+  Serial.println((String)"pin A2: " + digitalRead(A2) + " V");
+  nAdjI = nAdjCurrentSensor.getCurrentDC();
+  Serial.println((String)"nAdjI: (DC) = " + nAdjI + " A");
+  
   I = adjI + nAdjI;
+  Serial.println((String)"adjI + nAdjI = I: = " + I + " A");
+  
   calcAdjCurrentLimit();
+
+  Serial.print((String)"float adjCurrentLimit = " + adjCurrentLimit + " A");
+  Serial.println((String)"int maxCurrent = " + maxCurrent + " A");
+  Serial.println((String)"int alertCurrent = " + alertCurrent + " A");
+  
 }
 
 void adjButtonToggle() {
   bool state = adjButtonValue;
   if(state == true) {
     adjButtonValue == false;
+    Serial.println((String)"digitalRead(10) (adjButton) = " + digitalRead(10));
   } else {
     adjButtonValue == true;
+    Serial.println((String)"digitalRead(10) (adjButton) = " + digitalRead(10));
   }
 }
 
@@ -229,40 +268,50 @@ void nAdjButtonToggle() {
   bool state = nAdjButtonValue;
   if(state == true) {
     nAdjButtonValue == false;
+    Serial.println((String)"digitalRead(11) (nAdjButton) = " + digitalRead(11));
   } else {
     nAdjButtonValue == true;
+    Serial.println((String)"digitalRead(11) (nAdjButton) = " + digitalRead(11));
   }
 }
 
 void relays() {
     while(adjButtonValue) {
     digitalWrite(adjRelay, HIGH);
+    Serial.println((String)"digitalWrite(7) (adjRelay) is HIGH? = " + digitalRead(11));
   }
 
   while(nAdjButtonValue) {
     digitalWrite(nAdjRelay, HIGH);
+    Serial.println((String)"digitalWrite(8) (nAdjRelay) is HIGH? = " + digitalRead(8));
   }
 }
 
-
 void loop() {
+  Serial.println("New loop: ");
+  Serial.println("messurment()");
   messurment();
+  Serial.println("safe()");
   safe();
 
+  Serial.println("read adjButton");
   int buttonA = digitalRead(adjButton);  // read input value
   if (buttonA == HIGH) {
     adjButtonToggle();
     alert = false;
   }
 
+  Serial.println("nAdjButton");
   int buttonB = digitalRead(nAdjButton);  // read input value
   if (buttonB == HIGH) {
     nAdjButtonToggle();
     alert = false;
   }
 
+  Serial.println("relays()");
   relays();
 
+  Serial.println("LEDs");
   if (adjButtonValue || nAdjButtonValue) {
     colorCalc(); 
   } else if (alert == true) {
@@ -272,4 +321,9 @@ void loop() {
   }
   
   delay(100);
+  
+  Serial.println("Loop ended");
+  
+  Serial.println("\n - - - \n");
+    
 }
